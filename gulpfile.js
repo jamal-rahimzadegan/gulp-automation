01118d9d736fb2f6e1195d849f8b98bcc7a63407
Jamal Rahimzadegan <jamalr@amdocs.com>
2022-10-24 13:48:29 +0300

    Feat: copy command

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ gulp.task("scripts", async () => {
   return await gulp.src("src/*.js").pipe(uglify()).pipe(gulp.dest("build/js"));
 });
 
-gulp.task("watch", function () {
+gulp.task("watch", () => {
   gulp.watch("src/*.js", gulp.series("scripts"));
+});
+
+gulp.task("copy", async () => {
+  await gulp
+    .src("./src/components/template/*")
+    .pipe(gulp.dest("./src/components/new-template"));
 });
 
 gulp.task("default", gulp.series("scripts", "watch"));
